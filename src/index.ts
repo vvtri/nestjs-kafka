38c00256d0ef1761fe30bfcae86ca3612eb8af30
Van Tri <vtri2016@gmail.com>
2023-05-07 17:30:42 +0700

    retry
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export { KafkaConfig, ConsumerConfig, ProducerConfig } from 'kafkajs';
 export { SchemaRegistryAPIClientArgs } from '@kafkajs/confluent-schema-registry/dist/api';
 
-// Main 1 2
+// Main 1 2 3 4
 export * from './data/index';
 export * from './decorators/index.decorator';
 export * from './interfaces/index.interface';
